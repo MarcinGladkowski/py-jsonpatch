c413f0b8bd7587ce2b6cf11653521e027a31955d
MarcinGladkowski <marcingladkowski@gmail.com>
2022-03-31 21:03:11 +0200

    test: create one instance of add operation

--- tests/test_patch_lib.py
+++ tests/test_patch_lib.py
@@ -3,24 +3,22 @@ from patch_lib import JsonPath, Add
 
 class TestAdd:
 
+    add = Add()
+
     def test_not_modified_document(self):
         json_path = JsonPath()
         document = json_path.patch_document({"test": "test"}, [])
         assert document == {"test": "test"}
 
     def test_add_operation_to_empty_document(self):
-        add = Add()
-        assert add.execute({}, {"path": "foo", "value": "bar"}) == {"foo": "bar"}
+        assert self.add.execute({}, {"path": "foo", "value": "bar"}) == {"foo": "bar"}
 
     def test_add_operation_to_not_empty_document(self):
-        add = Add()
-        assert add.execute({"test": "test"}, {"path": "foo", "value": "bar"}) == {"test": "test", "foo": "bar"}
+        assert self.add.execute({"test": "test"}, {"path": "foo", "value": "bar"}) == {"test": "test", "foo": "bar"}
 
     def test_add_operation_to_array_value(self):
-        add = Add()
-        assert add.execute({"foo": ["baz"]}, {"path": "foo", "value": "bar"}) == {"foo": ["baz", "bar"]}
+        assert self.add.execute({"foo": ["baz"]}, {"path": "foo", "value": "bar"}) == {"foo": ["baz", "bar"]}
 
     def test_add_operation_nested_path(self):
-        add = Add()
-        result = add.execute({}, {"path": "foo/bar", "value": "baz"})
+        result = self.add.execute({}, {"path": "foo/bar", "value": "baz"})
         assert result == {"foo": {"bar": "baz"}}
