b56fb8a38a79f379529b247bc8c00418a2968f51
MarcinGladkowski <marcingladkowski@gmail.com>
2022-04-01 07:14:08 +0200

    tests: add front slash

--- tests/test_patch_lib.py
+++ tests/test_patch_lib.py
@@ -13,16 +13,18 @@ class TestAdd:
         assert document == {"test": "test"}
 
     def test_add_operation_to_empty_document(self):
-        assert self.add.execute({}, {"path": "foo", "value": "bar"}) == {"foo": "bar"}
+        assert self.add.execute({}, {"path": "/foo", "value": "bar"}) == {"foo": "bar"}
 
     def test_add_operation_to_not_empty_document(self):
-        assert self.add.execute({"test": "test"}, {"path": "foo", "value": "bar"}) == {"test": "test", "foo": "bar"}
+        assert self.add.execute({"test": "test"}, {"path": "/foo", "value": "bar"}) == {"test": "test", "foo": "bar"}
 
     def test_add_operation_to_array_value(self):
-        assert self.add.execute({"foo": ["baz"]}, {"path": "foo", "value": "bar"}) == {"foo": ["baz", "bar"]}
+        assert self.add.execute({"foo": ["baz"]}, {"path": "/foo", "value": "bar"}) == {"foo": ["baz", "bar"]}
 
     def test_add_operation_nested_path(self):
         """incorrect operation, shouldn't create elements recursively"""
         with pytest.raises(PathError):
-            self.add.execute({}, {"path": "foo/bar", "value": "baz"})
+            self.add.execute({}, {"path": "/foo/bar", "value": "baz"})
+
+
 
